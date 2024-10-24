1c4e081742df2fa96018fb9499110d66ef384c34
Patrick Coutinho <rodrigo@MacBook-Pro-de-Rodrigo.local>
2024-10-24 17:43:09 -0300

    update in password

--- db/connection.php
+++ db/connection.php
@@ -1,7 +1,12 @@
 <?php
-$conn = new mysqli('localhost', 'username', 'password', 'test');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Erro de conexão: " . $conn->connect_error);
+    die("Falha na conexão: " . $conn->connect_error);
 }
 ?>
